205ab5e5b87098c3086bde701fc2f815b7bb6a32
Joaquin Andres Machuca Martinez <151282523+JoakoAM@users.noreply.github.com>
2025-09-10 15:22:48 +0000

    prueba con replaceChildren

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function renderCategories() {
 // Cargar datos públicos (categorías y herramientas aprobadas)
 function loadPublicData() {
   const categoriesView = document.getElementById('categoriesView');
-  categoriesView.insertAdjacentHTML("beforeend", `<div class="loading">Cargando categorías...</div>`);
+  categoriesView.replaceChildren(`<div class="loading">Cargando categorías...</div>`);
   // Cargar categorías aprobadas
   db.collection("categories").where("approved", "==", true)
     .get()
